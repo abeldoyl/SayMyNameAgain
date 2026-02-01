5bbcbfae146d78bdf20378358067d6bfb1973ca2
abeldoyl <abeldoyle71@gmail.com>
2026-02-01 15:53:58 -0700

    add for loop

--- SayMyNameAgain/Program.cs
+++ SayMyNameAgain/Program.cs
@@ -25,47 +25,50 @@ namespace SayMyName
             }
             else if (userInput == "Abel")
             {
-                Console.WriteLine("Hey Abel!");
-                Console.Beep(293, 150);
-                Console.Beep(293, 150);
-                Console.Beep(587, 250);
-                Console.Beep(440, 360);
-                Console.Beep(415, 250);
-                Console.Beep(392, 250);
-                Console.Beep(349, 250);
-                Console.Beep(293, 130);
-                Console.Beep(349, 130);
-                Console.Beep(392, 130);
-                Console.Beep(261, 140);
-                Console.Beep(261, 120);
-                Console.Beep(587, 250);
-                Console.Beep(440, 370);
-                Console.Beep(415, 250);
-                Console.Beep(392, 250);
-                Console.Beep(349, 250);
-                Console.Beep(293, 130);
-                Console.Beep(349, 130);
-                Console.Beep(392, 130);
-                Console.Beep(246, 140);
-                Console.Beep(246, 120);
-                Console.Beep(587, 250);
-                Console.Beep(440, 370);
-                Console.Beep(415, 250);
-                Console.Beep(392, 240);
-                Console.Beep(349, 250);
-                Console.Beep(293, 120);
-                Console.Beep(349, 120);
-                Console.Beep(392, 130);
-                Console.Beep(233, 140);
-                Console.Beep(233, 120);
-                Console.Beep(587, 250);
-                Console.Beep(440, 370);
-                Console.Beep(415, 260);
-                Console.Beep(392, 250);
-                Console.Beep(349, 250);
-                Console.Beep(293, 120);
-                Console.Beep(349, 120);
-                Console.Beep(392, 130);
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("Hey Abel!");
+                    Console.Beep(293, 150);
+                    Console.Beep(293, 150);
+                    Console.Beep(587, 250);
+                    Console.Beep(440, 360);
+                    Console.Beep(415, 250);
+                    Console.Beep(392, 250);
+                    Console.Beep(349, 250);
+                    Console.Beep(293, 130);
+                    Console.Beep(349, 130);
+                    Console.Beep(392, 130);
+                    Console.Beep(261, 140);
+                    Console.Beep(261, 120);
+                    Console.Beep(587, 250);
+                    Console.Beep(440, 370);
+                    Console.Beep(415, 250);
+                    Console.Beep(392, 250);
+                    Console.Beep(349, 250);
+                    Console.Beep(293, 130);
+                    Console.Beep(349, 130);
+                    Console.Beep(392, 130);
+                    Console.Beep(246, 140);
+                    Console.Beep(246, 120);
+                    Console.Beep(587, 250);
+                    Console.Beep(440, 370);
+                    Console.Beep(415, 250);
+                    Console.Beep(392, 240);
+                    Console.Beep(349, 250);
+                    Console.Beep(293, 120);
+                    Console.Beep(349, 120);
+                    Console.Beep(392, 130);
+                    Console.Beep(233, 140);
+                    Console.Beep(233, 120);
+                    Console.Beep(587, 250);
+                    Console.Beep(440, 370);
+                    Console.Beep(415, 260);
+                    Console.Beep(392, 250);
+                    Console.Beep(349, 250);
+                    Console.Beep(293, 120);
+                    Console.Beep(349, 120);
+                    Console.Beep(392, 130);
+                }
             }
             else if (userInput == "Tim")
             {
