e7012c655ef6fd59d9eab77c624fd04eeac35721
abeldoyl <abeldoyle71@gmail.com>
2026-02-01 11:09:47 -0700

    add orignal SayMyName

--- SayMyNameAgain/Program.cs
+++ SayMyNameAgain/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace SayMyNameAgain
+﻿/*
+Abel Doyle
+2265
+Spring Semester 2026
+SayMyName
+https://github.com/abeldoyl/SayMyName.git
+*/
+
+namespace SayMyName
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput = "name";
+
+            Console.WriteLine("What's your Name");
+            userInput = Console.ReadLine();
+            Console.WriteLine($"Hello {userInput}");
+
+            Console.Read();
         }
     }
 }
